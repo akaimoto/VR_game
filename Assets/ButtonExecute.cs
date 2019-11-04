24eb5e53dc5a40de02dde0243e87ba0d4f2ef03c
akaimoto <endaikouen@yahoo.co.jp>
2019-11-04 23:44:03 +0900

    buttonのハイライト途中

---
+++ Assets/ButtonExecute.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonExecute : MonoBehaviour
+{
+    private GameObject currentButton;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform camera = Camera.main.transform;
+        Ray ray = new Ray(camera.position, camera.rotation * Vector3.forward);
+        RaycastHit hit;
+        GameObject hitButton = null;
+        PointerEventData data = new PointerEventData(EventSystem.current);
+        if(Physics.Raycast(ray, out hit))
+        {
+            if(hit.transform.gameObject.tag == "Button")
+            {
+                hitButton = hit.transform.parent.gameObject;
+            }
+        }
+        if (currentButton != hitButton)
+        {
+            if (currentButton != null)
+            { //ハイライトを外す
+                ExecuteEvents.Execute<IPointerExitHandler>(currentButton, data, ExecuteEvents.pointerExitHandler);
+            }
+
+            currentButton = hitButton;
+            if (currentButton != null)
+            {  //ハイライトする
+                ExecuteEvents.Execute<IPointerEnterHandler>(currentButton, data, ExecuteEvents.pointerEnterHandler);
+            }
+        }
+    }
+}
