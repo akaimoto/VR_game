c1e54bd8afbfb22b94845152559d8831c3630afc
akaimoto <endaikouen@yahoo.co.jp>
2019-11-17 17:51:09 +0900

    一人称ゲームの試作 カメラの一人称化

---
+++ Assets/HeadLookWalk.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadLookWalk : MonoBehaviour {
+    public float velocity = 0.7f;
+	// Update is called once per frame
+	void Update () {
+        Vector3 moveDirection = Camera.main.transform.forward;
+        moveDirection *= velocity * Time.deltaTime;
+        moveDirection.y = 0.0f;
+        transform.position += moveDirection; 
+	
+	}
+}
