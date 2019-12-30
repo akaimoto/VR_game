68574d46546371cb8ebb36ffb8d619ffc9db2177
akaimoto <endaikouen@yahoo.co.jp>
2019-12-30 23:55:54 +0900

    首振りによる移動の開始・終了を実装

--- Assets/HeadLookWalk.cs
+++ Assets/HeadLookWalk.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class HeadLookWalk : MonoBehaviour {
     public float velocity = 0.7f;
-	// Update is called once per frame
-	void Update () {
-        Vector3 moveDirection = Camera.main.transform.forward;
-        moveDirection *= velocity * Time.deltaTime;
-        moveDirection.y = 0.0f;
-        transform.position += moveDirection; 
-	
-	}
+    public bool isWalking = false;
+
+    private CharacterController controller;
+    private Clicker clicker;
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    // Update is called once per frame
+    void Update () {
+        if (clicker.clicked())
+        {
+            isWalking = !isWalking;
+        }
+        if (isWalking)
+        {
+            Vector3 moveDirection = Camera.main.transform.forward;
+            moveDirection *= velocity * Time.deltaTime;
+            moveDirection.y = 0.0f;
+            transform.position += moveDirection;
+            controller.Move(moveDirection);
+        }
+    }
 }
